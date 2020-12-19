d8076ef04a6d1292cfcc47cf382ba45cec68c7e2
Your Name <you@example.com>
2020-12-19 16:54:07 +0600

    added

---
+++ #source/js/bundle.js
@@ -0,0 +1,16 @@
+//@prepros-append plugins/jquery-3.5.1.min.js
+//@prepros-append plugins/slick.min.js
+//@prepros-append plugins/jquery.fullPage.min.js
+//@prepros-append plugins/isotope.pkgd.min.js
+
+
+
+
+
+
+
+
+
+
+
+
